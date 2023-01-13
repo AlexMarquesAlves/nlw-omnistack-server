73bd08bc2fd4ab3012f9ae6bd9b9759bf65f0eeb
¡Duque <atanaelleonardo@gmail.com>
2023-01-13 13:59:58 -0300

    feat: added static image

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -54,7 +54,8 @@ export class PointsController {
     const transactions = await knex.transaction();
 
     const point = {
-      image: "image-fake",
+      image:
+        "https://images.unsplash.com/photo-1562077981-4d7eafd44932?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60",
       name,
       email,
       whatsapp,

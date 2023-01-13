be51e1781856a822c98faed9628a9ce6db36d9fb
¡Duque <atanaelleonardo@gmail.com>
2023-01-13 02:52:18 -0300

    chore: created get /items route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,22 @@
 import * as express from "express";
 import { Request, Response } from "express";
+import knex from "../database/connection";
 
 const routes = express.Router();
+const tableName = "items";
+const static_url = "http://localhost:3333";
+const image_base_url = `${static_url}/uploads`;
 
-routes.get("/", (req: Request, res: Response) => {
-  return res.json({ message: "Hello World!" });
+routes.get("/items", async (req: Request, res: Response) => {
+  const items = await knex(tableName).select("*");
+  const serializedItems = items.map((item) => {
+    return {
+      title: item.title,
+      image_url: `${image_base_url}/${item.image}`,
+    };
+  });
+
+  return res.json(serializedItems);
 });
 
 export default routes;

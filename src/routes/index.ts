fe1f60a90b562c1b3cae4cd526bf3e437e3ee923
¡Duque <atanaelleonardo@gmail.com>
2023-01-13 03:06:10 -0300

    chore: created post /points route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,20 +3,51 @@ import { Request, Response } from "express";
 import knex from "../database/connection";
 
 const routes = express.Router();
-const tableName = "items";
+const tableItems = "items";
+const tablePoints = "points";
+const tablePointItems = "point_items";
 const static_url = "http://localhost:3333";
 const image_base_url = `${static_url}/uploads`;
 
 routes.get("/items", async (req: Request, res: Response) => {
-  const items = await knex(tableName).select("*");
+  const items = await knex(tableItems).select("*");
   const serializedItems = items.map((item) => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `${image_base_url}/${item.image}`,
     };
   });
 
   return res.json(serializedItems);
+});
+
+routes.post("/points", async (req: Request, res: Response) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { name, email, whatsapp, latitude, longitude, city, uf, items } =
+    req.body;
+
+  const ids = await knex(tablePoints).insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id: ids[0],
+    };
+  });
+
+  await knex(tablePointItems).insert(pointItems);
+
+  return res.json({ success: true });
 });
 
 export default routes;
